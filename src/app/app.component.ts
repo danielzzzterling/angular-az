1d36374d40274adb2f35ed6f9c3a9321521a8961
Alexander Zabaleta <jhon.alexanderzc@gmail.com>
2024-11-19 18:40:06 -0500

    Sliders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SobreNosotrosComponent} from './sobre-nosotros/sobre-nosotros.component';
 import { FooderComponent } from './fooder/fooder.component';
+import { SliderComponent } from './slider/slider.component';
+import { SliderCardsComponent } from './slider-cards/slider-cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SobreNosotrosComponent,FooderComponent],
+  imports: [RouterOutlet,SobreNosotrosComponent,FooderComponent,SliderComponent,SliderCardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
