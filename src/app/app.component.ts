aa958d29d0e2450b0356781bd59577744464c9ca
Daniel <rodriguezsterlingd@gmail.com>
2024-11-21 16:50:31 -0500

    create new folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SobreNosotrosComponent} from './sobre-nosotros/sobre-nosotros.component';
-import { FooderComponent } from './fooder/fooder.component';
-import { SliderComponent } from './slider/slider.component';
-import { SliderCardsComponent } from './slider-cards/slider-cards.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { SobreNosotrosComponent} from './components/sobre-nosotros/sobre-nosotros.component';
+import { FooderComponent } from './core/fooder/fooder.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SliderCardsComponent } from './components/slider-cards/slider-cards.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,SobreNosotrosComponent,FooderComponent,NavbarComponent,SliderComponent,SliderCardsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss' 
 })
 export class AppComponent {
   title = 'proyecto-final';
