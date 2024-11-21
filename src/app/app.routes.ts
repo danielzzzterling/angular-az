b31accc6402732aa71898317a240de209416b879
Miguel <chauxmiguel83@gmail.com>
2024-11-21 18:52:55 -0500

    21-11-2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { CamisetasComponent } from './layouts/product-list/camisetas.component';
 
-export const routes: Routes = [
-    { path: 'camisetas', component: CamisetasComponent },
-]; 
+
+export const routes: Routes = []; 
