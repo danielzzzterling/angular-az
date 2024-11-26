9d9e513c09549c96826eee979b9246e3793832da
Daniel <rodriguezsterlingd@gmail.com>
2024-11-25 20:17:37 -0500

    api link with az, creation details and use product list

--- src/app/layouts/product-list/product-list.component.ts
+++ src/app/layouts/product-list/product-list.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { ProductsService } from '../../../services/products.service';
 
-interface  Product {
+export interface Product {
   id: number,
   urlSlug: string,
-  titulo: string,
-  texto: string,
-  imagen: string,  
+  product_name: string,
+  description: string,
+  image_url: string,
+  price: number;
+  stock_quantity: number;
 }
 
 @Component({
@@ -15,39 +18,31 @@ interface  Product {
   standalone: true,
   imports: [CommonModule,ProductCardComponent],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
+  providers: [ProductsService]
 })
 export class ProductListComponent implements OnInit{
+  
 
-  products: Product[] = [] as Product[]
+  
+  productList: Product[] = [] as Product[]
 
-  ngOnInit(): void {
-    this.products = [
-      {
-        id: 1,
-        urlSlug: 'producto_1',
-        titulo: 'Producto 1',
-        texto: 'Descripción del producto 1.',
-        imagen: 'https://via.placeholder.com/150',
+  
+
+  
+    constructor(private productsService: ProductsService) { }
+  
+    ngOnInit() {
+      this.productsService.getProducts().subscribe(products  => {
+        this.productList = products; 
+        console.log(this.productList)
         
-      },
-      {
-        id: 2,
-        urlSlug: 'producto_2',
-        titulo: 'Producto 2',
-        texto: 'Descripción del producto 2.',
-        imagen: 'https://via.placeholder.com/150',
-      },
-      {
-        id: 3,
-        urlSlug: 'producto_3',
-        titulo: 'Producto 3',
-        texto: 'Descripción del producto 3.',
-        imagen: 'https://via.placeholder.com/150',
-      }
-    ];
-    
+      });
+    }
   }
-  }
+  
+  
+  
+  
 
 
